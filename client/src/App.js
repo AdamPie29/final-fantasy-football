f42448d9a1617bd6b0c9098de7091e821781c5bf
Adam Pietrangelo <apietrangelo44@gmail.com>
2022-12-06 22:27:43 -0500

    creates header component with styling for mobile

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import './App.scss';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <BrowserRouter>
-    {/* <Header /> */}
+    <Header />
     <main className='page-container'>
       <Routes>
         {/* <Route path="/" element={<Homepage/>} /> */}
