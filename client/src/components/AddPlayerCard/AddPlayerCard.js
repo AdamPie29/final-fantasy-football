8ae41a20ae44c25e276f5f030029e9ec17942b29
Adam Pietrangelo <apietrangelo44@gmail.com>
2022-12-13 23:31:59 -0500

    adds styling to added player card on create team page

--- client/src/components/AddPlayerCard/AddPlayerCard.js
+++ client/src/components/AddPlayerCard/AddPlayerCard.js
@@ -3,13 +3,12 @@ import "./AddPlayerCard.scss";
 function AddPlayerCard ({props}) {
 
     return (
-        <div className="player-row">
-            <div className="player-row__player">
-                <img className="player-row__player__img" src={props.PhotoUrl} alt="Player " />
-                <div className="player-row__player__details">
-                    <p className="player-row__player__details__name">{props.FirstName + ' ' + props.LastName}</p>
-                    <p className="player-row__player__details__team">{props.Team} -</p>
-                    <p className="player-row__player__details__position">{props.Position}</p>
+        <div className="add-player-row">
+            <div className="add-player-row__player">
+                <img className="add-player-row__player__img" src={props.PhotoUrl} alt="Player " />
+                <div className="add-player-row__player__details">
+                    <p className="add-player-row__player__details__name">{props.FirstName + ' ' + props.LastName}</p>
+                    <p className="add-player-row__player__details__position">{props.Position}</p>
                 </div>
             </div>
         </div>
